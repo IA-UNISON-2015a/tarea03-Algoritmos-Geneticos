f8a6ced36c5656b3ec649ea447c1f60364469211
robertosalazare <jrse95@gmail.com>
2017-10-01 22:00:01 -0700

    Agregados los metodos torneo, cadena a estado, estado a cadena y adaptacion

--- genetico_tarea.py
+++ genetico_tarea.py
@@ -11,6 +11,8 @@ genético para resolver problemas de permutaciones
 
 import random
 import genetico
+from random import randint
+import genetico_nreinas
 
 __author__ = 'Tu nombre'
 
@@ -31,43 +33,65 @@ class GeneticoPermutacionesPropio(genetico.Genetico):
         estado_a_cadena).
 
         """
-        self.nombre = 'propuesto por el alumno'
+        self.nombre = 'propuesto por Roberto Salazar'
         super().__init__(problema, n_población)
         #
         # ------ IMPLEMENTA AQUI TU CÓDIGO -----------------------------------
         #
 
+    def torneo(self):
+        n1 = randint(0,self.n_población)
+        n2 = randint(0,self.n_población)
+        while n2 == n1:
+            n2 = randint(0,self.n_población)
+        a1,a2 = self.población[n1][0],self.población[n2][0]
+        return n1 if a1 > a2 else n2
+
     @staticmethod
     def estado_a_cadena(estado):
         """
-        Convierte un estado a una cadena de cromosomas independiente
-        del problema de permutación
+        Convierte un estado a una cadena de cromosomas
 
         @param estado: Una tupla con un estado
         @return: Una lista con una cadena de caracteres
 
+        Por default converte el estado en una lista.
+
         """
-        #
-        # ------ IMPLEMENTA AQUI TU CÓDIGO --------------------------------
-        #
-        raise NotImplementedError("¡Este metodo debe ser implementado!")
+        #se genera una lista con los numeros de 0 a n
+        lista = list(range(len(estado)))
+        #se inicializa la cadena como una lista vacía
+        cadena = []
+        for v in range(len(estado) - 1):
+            #se busca la posición del elemento en la lista
+            i = lista.index(estado[v])
+            #se saca el elemento de la lista
+            lista.pop(i)
+            #se agrega el indice del elemento a la cadena
+            cadena.append(i)
+        print(estado,cadena)
+        return cadena
 
     @staticmethod
     def cadena_a_estado(cadena):
         """
-        Convierte una cadena de cromosomas a un estado donde el estado es
-        una posible solución a un problema de permutaciones
+        Convierte una cadena de cromosomas a un estado
 
         @param cadena: Una lista de cromosomas o valores
         @return: Una tupla con un estado válido
 
-        """
-        #
-        # ------ IMPLEMENTA AQUI TU CÓDIGO --------------------------------
-        #
-        raise NotImplementedError("¡Este metodo debe ser implementado!")
+        Por default convierte la lista a tupla
 
-        
+        """
+        lista = list(range(len(cadena) + 1))
+        estado = []
+        for i in cadena:
+            v = lista[i]
+            lista.pop(i)
+            estado.append(v)
+        estado.append(lista[0])
+        return estado
+
     def adaptación(self, individuo):
         """
         Calcula la adaptación de un individuo al medio, mientras más adaptado
@@ -76,11 +100,9 @@ class GeneticoPermutacionesPropio(genetico.Genetico):
         @param individuo: Una lista de cromosomas
         @return un número con la adaptación del individuo
 
+        Por default usa 1 / (costo(estado) + 1)
         """
-        #
-        # ------ IMPLEMENTA AQUI TU CÓDIGO --------------------------------
-        #
-        raise NotImplementedError("¡Este metodo debe ser implementado!")
+        return 1 / (1.0 + self.problema.costo(self.cadena_a_estado(individuo)))
 
     def selección(self):
         """
@@ -142,7 +164,9 @@ class GeneticoPermutacionesPropio(genetico.Genetico):
 
 
 if __name__ == "__main__":
+    genetico = GeneticoPermutacionesPropio(genetico_nreinas.ProblemaNreinas(8), 10)
+    print(genetico.población[0])
     # Un objeto genético con permutaciones con una población de
     # 10 individuos y una probabilidad de mutacion de 0.1
-    g_propio = GeneticoPermutacionesPropio(genetico.ProblemaTonto(10), 10)
-    genetico.prueba(g_propio)
+    # g_propio = GeneticoPermutacionesPropio(genetico.ProblemaTonto(10), 10)
+    # genetico.prueba(g_propio)
